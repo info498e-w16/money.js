9190b6662b45b9a6cacaf11a90a1c7f1a37aa808
Joel Ross <joelross@uw.edu>
2016-01-12 09:30:13 -0800

    Cleaned for commenting exercise (untested)

--- money.js
+++ money.js
@@ -7,47 +7,44 @@
  * Freely distributable under the MIT license.
  * Portions of money.js are inspired by or borrowed from underscore.js
  *
- * For details, examples and documentation:
- * http://openexchangerates.github.io/money.js/
+ * This code has been modified slightly for the assignment at hand.
+ * 
+ * YOUR TASK: add comments to this code!
+ *  - you'll need to practice reading the language to understand it
+ *  - make sure to describe the parameters and return values of each method!
  */
-(function(root, undefined) {
+(function(root, undefined) { //creates an anonymous function to "contain" the code. Passed a reference to the "root" scope (e.g., a global object we might want to use). Root will be `window` in browser or `global` on the server:
 
-	// Create a safe reference to the money.js object for use below.
+  //is fx an object or a function (OR BOTH?!)
 	var fx = function(obj) {
 		return new fxWrapper(obj);
 	};
 
-	// Current version.
+
 	fx.version = '0.2';
 
 
-	/* --- Setup --- */
+  /* Setup */
 
-	// fxSetup can be defined before loading money.js, to set the exchange rates and the base
-	// (and default from/to) currencies - or the rates can be loaded in later if needed.
 	var fxSetup = root.fxSetup || {
 		rates : {},
 		base : ""
 	};
 
-	// Object containing exchange rates relative to the fx.base currency, eg { "GBP" : "0.64" }
+
 	fx.rates = fxSetup.rates;
 
-	// Default exchange rate base currency (eg "USD"), which all the exchange rates are relative to
-	fx.base = fxSetup.base;
+	fx.baseRate = fxSetup.baseRate;
 
-	// Default from / to currencies for conversion via fx.convert():
 	fx.settings = {
-		from : fxSetup.from || fx.base,
-		to : fxSetup.to || fx.base
+		from : fxSetup.from || fx.baseRate,
+		to : fxSetup.to || fx.baseRate
 	};
 
 
 	/* --- Conversion --- */
 
-	// The base function of the library: converts a value from one currency to another
 	var convert = fx.convert = function(val, opts) {
-		// Convert arrays recursively
 		if (typeof val === 'object' && val.length) {
 			for (var i = 0; i< val.length; i++ ) {
 				val[i] = convert(val[i], opts);
@@ -55,43 +52,36 @@
 			return val;
 		}
 
-		// Make sure we gots some opts
 		opts = opts || {};
 
-		// We need to know the `from` and `to` currencies
 		if( !opts.from ) opts.from = fx.settings.from;
 		if( !opts.to ) opts.to = fx.settings.to;
 
-		// Multiple the value by the exchange rate
 		return val * getRate( opts.to, opts.from );
 	};
 
-	// Returns the exchange rate to `target` currency from `base` currency
+
 	var getRate = function(to, from) {
-		// Save bytes in minified version
 		var rates = fx.rates;
 
-		// Make sure the base rate is in the rates object:
-		rates[fx.base] = 1;
+		rates[fx.baseRate] = 1;
 
-		// Throw an error if either rate isn't in the rates array
 		if ( !rates[to] || !rates[from] ) throw "fx error";
 
-		// If `from` currency === fx.base, return the basic exchange rate for the `to` currency
-		if ( from === fx.base ) {
+		if ( from === fx.baseRate ) {
 			return rates[to];
 		}
 
-		// If `to` currency === fx.base, return the basic inverse rate of the `from` currency
-		if ( to === fx.base ) {
+		if ( to === fx.baseRate ) {
 			return 1 / rates[from];
 		}
 
-		// Otherwise, return the `to` rate multipled by the inverse of the `from` rate to get the
-		// relative exchange rate between the two currencies
 		return rates[to] * (1 / rates[from]);
 	};
 
+
+
+  /** The below code is already commented; but you might read through it to make sure you follow what is going on! **/
 
 	/* --- OOP wrapper and chaining --- */
 
@@ -117,9 +107,9 @@
 	};
 
 	// fx(val).from(currency) returns a wrapped `fx` where the value has been converted from
-	// `currency` to the `fx.base` currency. Should be followed by `.to(otherCurrency)`
+	// `currency` to the `fx.baseRate` currency. Should be followed by `.to(otherCurrency)`
 	fxProto.from = function(currency) {
-		var wrapped = fx(convert(this._v, {from: currency, to: fx.base}));
+		var wrapped = fx(convert(this._v, {from: currency, to: fx.baseRate}));
 		wrapped._fx = fx.base;
 		return wrapped;
 	};
@@ -132,35 +122,10 @@
 
 	/* --- Module Definition --- */
 
-	// Export the fx object for CommonJS. If being loaded as an AMD module, define it as such.
-	// Otherwise, just add `fx` to the global object
 	if (typeof exports !== 'undefined') {
 		if (typeof module !== 'undefined' && module.exports) {
 			exports = module.exports = fx;
 		}
 		exports.fx = fx;
-	} else if (typeof define === 'function' && define.amd) {
-		// Return the library as an AMD module:
-		define([], function() {
-			return fx;
-		});
-	} else {
-		// Use fx.noConflict to restore `fx` back to its original value before money.js loaded.
-		// Returns a reference to the library's `fx` object; e.g. `var money = fx.noConflict();`
-		fx.noConflict = (function(previousFx) {
-			return function() {
-				// Reset the value of the root's `fx` variable:
-				root.fx = previousFx;
-				// Delete the noConflict function:
-				fx.noConflict = undefined;
-				// Return reference to the library to re-assign it:
-				return fx;
-			};
-		})(root.fx);
 
-		// Declare `fx` on the root (global/window) object:
-		root['fx'] = fx;
-	}
-
-	// Root will be `window` in browser or `global` on the server:
 }(this));
